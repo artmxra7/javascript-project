59afc4d095b55750d485ac7cf8b7c1ccf075e701
Erwin Rahayu <artmxarea@gmail.com>
2019-06-25 20:40:20 +0700

    Array di Javascript

---
+++ pertermuan-9/assets/js/main.js
@@ -0,0 +1,12 @@
+// membuat variable
+
+
+// Fungsi array
+function jumlahinput(){
+
+  var data = ["Dimas", "Sudrajat", "Jakarta", "09021998"];
+ 
+  document.getElementById("wrap").innerHTML = data.join(" ");   
+}
+
+
