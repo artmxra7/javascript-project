7a23f0f0b39e7d752df5e9f1169c397d62d9d30b
Erwin Rahayu <artmxarea@gmail.com>
2019-06-25 17:29:25 +0700

    Return Dengan Parameter

---
+++ pertemuan-8/assets/js/main.js
@@ -0,0 +1,11 @@
+// membuat variable
+
+
+// Fungsi return
+function jumlahinput(input, input1){
+
+  return input + input1;
+}
+
+var output = jumlahinput(10, 20);
+document.getElementById("wrap").innerHTML = output;   
