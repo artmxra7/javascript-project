f45572eb4e6355173caef3345b53c24f31450dd3
Erwin Rahayu <artmxarea@gmail.com>
2019-06-25 15:03:35 +0700

    Menggabungkan 2 Variabel di JAVASCRIPT

---
+++ pertemuan-3/assets/js/main.js
@@ -0,0 +1,6 @@
+// membuat variable
+var awal = 2000;
+var akhir = 300;
+
+var total_pengeluaran = awal - akhir;
+document.getElementById("wrap").innerHTML = "Total Pengeluaran " + total_pengeluaran;      
