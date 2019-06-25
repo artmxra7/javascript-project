082b99c33dec4666e91c05b6ffcf6f17c6b03877
Erwin Rahayu <artmxarea@gmail.com>
2019-06-25 16:25:35 +0700

    Membuat fungsi dan Memanggil Fungsi di Javascript

---
+++ pertemuan-4/assets/js/main.js
@@ -0,0 +1,11 @@
+// membuat variable
+var awal = 2000;
+var akhir = 300;
+
+// Membuat Fungsi
+function printText(){
+  alert("Input Selesai");
+  var total_pengeluaran = awal - akhir;
+  document.getElementById("wrap").innerHTML = "Total Pengeluaran " + total_pengeluaran;   
+}
+   
